46415062f286eff666bacc54bdb568a849d7345f
federico-paganini <perfedefer@gmail.com>
2023-08-12 21:46:03 -0300

    Verificación de logueo y redireccion
    Se agrega la función para redirigir luego de apretar el boton submit

--- js/login.js
+++ js/login.js
@@ -17,3 +17,32 @@ btnreg.addEventListener("click", function () {
 
 formlog.style.display = "block";
 formreg.style.display = "none";
+
+
+/* Redireccion a index con login realizado */
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const btnsub = document.getElementById("btn-submitlog");
+    const form = document.querySelector("#form-log form");
+
+    btnsub.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const user = form.querySelector("#User");
+        const pass = form.querySelector("#Pass");
+
+        if (user.value && pass.value) {
+            alert("Logueado correctamente");
+            setTimeout(function () {
+                window.location.href = "index.html";
+            }, 2000);
+        } else { 
+            if (!user.value) {
+                alert("Debes llenar el campo Usuario");
+            } else {
+            alert("Debes ingresar una contraseña");
+            }   
+        }
+        });
+});

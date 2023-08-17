d2077e6baa88847b2c45ef59f3252302bb217396
Agustina Fleitas <agustinaafd@gmail.com>
2023-08-17 12:00:58 -0300

    Arreglo barra superior
    Arreglo desplazamiento de los botones registro  e inicio de sesión

--- js/iniciarSesion_registro.js
+++ js/iniciarSesion_registro.js
@@ -4,17 +4,18 @@ const btnlog = document.getElementById("btn-log");
 const btnreg = document.getElementById("btn-reg");
 const formlog = document.getElementById("form-log");
 const formreg = document.getElementById("form-reg");
+const btn = document.getElementById("btn");
 
 btnlog.addEventListener("click", function () {
     formlog.style.left= "0";
     formreg.style.left = "775px";
-    button.style.left= "0";
+    btn.style.left= "0";
 })
 
 btnreg.addEventListener("click", () => {
     formlog.style.left= "-700px";
     formreg.style.left = "0px";
-    button.style.left= "270px";
+    btn.style.left= "270px";
 })
 
 
